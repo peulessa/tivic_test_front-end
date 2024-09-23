17a569c6ecebe8b3b01bcf6bb37337d78839c634
peulessa <pepeulessa@hotmail.com>
2024-09-23 10:06:18 -0300

    Ajustando logica de service de busca de dados da tabela

--- src/app/services/table.service.ts
+++ src/app/services/table.service.ts
@@ -7,11 +7,22 @@ import { TableData } from '../../TableData';
   providedIn: 'root',
 })
 export class TableService {
-  private apiUrl = 'localhost:3000/api/receitas';
+  private apiUrl = 'http://localhost:8080/api/acidentes';
+  private username = 'user';
+  private password = '4a986c8d-1ef1-41eb-9409-ff486245170c';
 
   constructor(private http: HttpClient) {}
 
+  private get authHeader(): HttpHeaders {
+    const credentials = btoa(`${this.username}:${this.password}`);
+    return new HttpHeaders({
+      Authorization: `Basic ${credentials}`,
+    });
+  }
+
   getTableData(): Observable<TableData[]> {
-    return this.http.get<TableData[]>(this.apiUrl);
+    return this.http.get<TableData[]>(this.apiUrl, {
+      headers: this.authHeader,
+    });
   }
 }

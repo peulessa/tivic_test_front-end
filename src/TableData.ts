fc2d5e255c991343935aaf4f3f4e0e94a33ab821
peulessa <pepeulessa@hotmail.com>
2024-09-23 12:17:27 -0300

    Ajuste para receber os dados da api

--- src/TableData.ts
+++ src/TableData.ts
@@ -1,22 +1,32 @@
 export interface TableData {
-  /*   uf: string;
-  cidade: string;
-  tipoAcidente: string;
-  causa: string;
-  data: Date;
-  hora: Date;
-  numeroDeVítimas: number; */
-
   id: number;
-  clienteId: number;
-  categoria: string;
-  dataRecebimento: string;
-  metodoPagamento: string;
-  valor: number;
-  descricao: string;
-  contaAReceberId: void;
-  contratoId: void;
-  createdAt: string;
-  updatedAt: string;
-  deletedAt: void;
+  data_inversa: string;
+  dia_semana: string;
+  horario: string;
+  uf: string;
+  br: string;
+  km: number;
+  municipio: string;
+  causa_acidente: string;
+  tipo_acidente: string;
+  classificacao_acidente: string;
+  fase_dia: string;
+  sentido_via: string;
+  condicao_metereologica: string;
+  tipo_pista: string;
+  tracado_via: string;
+  uso_solo: string;
+  pessoas: number;
+  mortos: number;
+  feridos_leves: number;
+  feridos_graves: number;
+  ilesos: number;
+  ignorados: number;
+  feridos: number;
+  veiculos: number;
+  latitude: number;
+  longitude: number;
+  regional: string;
+  delegacia: string;
+  uop: string;
 }
